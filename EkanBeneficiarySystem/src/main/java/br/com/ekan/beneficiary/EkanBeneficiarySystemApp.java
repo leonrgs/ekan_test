33385b5b29a7da0ae0208c2db4adbdfcc0f8cf87
Leonardo Santos de Andrade <leonrgs@gmail.com>
2023-12-12 16:20:16 -0300

    Inclusao de seguranca basica

--- EkanBeneficiarySystem/src/main/java/br/com/ekan/beneficiary/EkanBeneficiarySystemApp.java
+++ EkanBeneficiarySystem/src/main/java/br/com/ekan/beneficiary/EkanBeneficiarySystemApp.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "br.com.ekan.beneficiary.repository")
 @EntityScan(basePackages = "br.com.ekan.beneficiary.entity")
+@EnableWebSecurity
 public class EkanBeneficiarySystemApp {
 
 	public static void main(String[] args) {
